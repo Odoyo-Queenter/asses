d0a904dd1f397057ac7c6824508b69f68f3035ec
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-04-09 16:57:51 +0300

    first commit

---
+++ varriables.py
@@ -0,0 +1,40 @@
+#varriables
+
+
+#varriables are containers for storing data.
+Name = "Queenter Doe"
+Age = 10
+print(Name, Age)
+#print(Age)
+
+Brand,color,weight="Volvo","Grey",156
+print(Brand)
+print(weight)
+print(color)
+
+print(Brand, weight, color)
+
+#DATA TYPES
+#LIST changable
+Fruits = ["Banana","Orange","Pineapple","Banana",156,145.67]
+print(Fruits[0])
+#Fruits[0]= "Apple" 
+
+
+print(Fruits)
+
+#tuple unchanagable
+Cars = ("volvo","audi","toyota", 23,45.67)
+print(Cars[2])
+
+
+#range 
+#mostly used in loops
+for i in range(10):
+    print (i)
+
+#Dictionary
+#set  doesnt accept a duplicate
+set1 = {1,2,2,3,5,6,6}
+print(set1)  
+
